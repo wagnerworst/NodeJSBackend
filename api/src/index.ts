e40a30ccd96329c5f0ae02931de2821491826d3d
Wagner <wagner.worst@gmail.com>
2024-08-22 21:44:36 -0300

    feat: implementa cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,12 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import defaultRoutes from './routes/defaultRoutes'
 import departamentosRoutes from './routes/departamentosRoutes'
 import funcionariosRoutes from './routes/funcionariosRoutes'
+import cors from 'cors';
 
 const app = express();
 const port = 3030;
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(defaultRoutes);
